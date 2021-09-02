629f0883a48d888ce5ce5f50735b30e5fddaf324
Ido Ozeri <ido@anecdotes.ai>
2021-09-02 09:17:23 +0300

    Not using --workflow-url anymore, but rather several additional options that comprise the url for the user

--- remote-workflow-control.sh
+++ remote-workflow-control.sh
@@ -8,15 +8,16 @@
 # Variables
 DIR=$(dirname $0)
 PROG=$(basename $0)
-DEFAULT_WAIT_TIMEOUT_MINUTES=5
-REQUIRED_BINARIES="openssl zcat jq curl"
 
 SCRIPT_FULLPATH=$(cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd)
+
+# Load everything we need;
 source $SCRIPT_FULLPATH/functions || exit 1
+source $SCRIPT_FULLPATH/variables || exit 1
 
 # Process options
-SHORT_OPTS="ha:b:i:u:w:"
-LONG_OPTS="help,auth-token:,workflow-inputs:,workflow-branch:,workflow-url:,wait-timeout-minutes:"
+SHORT_OPTS="ha:b:i:w:o:r:y:"
+LONG_OPTS="help,auth-token:,workflow-inputs:,workflow-branch:,wait-timeout-minutes:,workflow-repo:,workflow-yaml:,workflow-org:"
 OPTS=$(getopt -o $SHORT_OPTS -l $LONG_OPTS -n "$0" -- "$@")
 
 if [ $? != 0 ] ; then
@@ -28,19 +29,19 @@ eval set -- "$OPTS"
 
 while true ; do
     case "$1" in
-        # Print the long options prefixed with '--';
         -h|--help) usage ; exit 0;;
         -a|--auth-token) AUTH_TOKEN=$(echo -n $2 | base64) ; shift 2;;
+        -o|--workflow-org) WORKFLOW_ORG=$2 ; shift 2;;
+        -r|--workflow-repo) WORKFLOW_REPO=$2 ; shift 2;;
+        -y|--workflow-yaml) WORKFLOW_YAML=$2 ; shift 2;;
         -b|--workflow-branch) WORKFLOW_BRANCH=$2 ; shift 2;;
         -i|--workflow-inputs) WORKFLOW_INPUTS=$2 ; shift 2;;
-        -u|--workflow-url) WORKFLOW_URL=$2 ; shift 2;;
         -w|--wait-timeout-minutes) WAIT_TIMEOUT_MINUTES=$2 ; shift 2;;
         --) shift; break;;
     esac
 done
 
 WAIT_TIMEOUT_MINUTES=${WAIT_TIMEOUT_MINUTES:-${DEFAULT_WAIT_TIMEOUT_MINUTES}}
-TRIGGER_UUID_INPUT_VAR="trigger_uuid"
 TRIGGER_UUID_VALUE=$(generate_uuid)
 
 ###########################################################################################
@@ -50,7 +51,9 @@ TRIGGER_UUID_VALUE=$(generate_uuid)
 check_required_binaries $REQUIRED_BINARIES || die 1 "cannot proceed when some binaries are missing"
 
 verify_option_was_provided AUTH_TOKEN      "--auth-token"
-verify_option_was_provided WORKFLOW_URL    "--workflow-url"
+verify_option_was_provided WORKFLOW_ORG    "--workflow-org"
+verify_option_was_provided WORKFLOW_REPO   "--workflow-repo"
+verify_option_was_provided WORKFLOW_YAML   "--workflow-yaml"
 verify_option_was_provided WORKFLOW_BRANCH "--workflow-branch"
 
 # Since the '--workflow-inputs' option requires *some* argument, 
@@ -62,8 +65,13 @@ if [[ $WORKFLOW_INPUTS == "none" ]] ; then
     declare WORKFLOW_INPUTS=""
 fi
 
-trigger_workflow $WORKFLOW_URL $WORKFLOW_BRANCH \
-    $TRIGGER_UUID_INPUT_VAR=$TRIGGER_UUID_VALUE ${WORKFLOW_INPUTS[@]}
+# Building the relevant URLs based on user input;
+WORKFLOW_BASE_URL="$GITHUB_API_BASEURL/$WORKFLOW_ORG/$WORKFLOW_REPO/actions"
+WORKFLOW_ARTIFACTS_URL="$WORKFLOW_BASE_URL/artifacts"
+WORKFLOW_DISPATCH_URL="$WORKFLOW_BASE_URL/workflows/$WORKFLOW_YAML/dispatches"
 
-wait_for_status $TRIGGER_UUID_VALUE $(extract_workflow_base_url $WORKFLOW_URL) $WAIT_TIMEOUT_MINUTES
+trigger_workflow $WORKFLOW_DISPATCH_URL $WORKFLOW_BRANCH \
+    $TRIGGER_UUID_INPUT_VAR=$TRIGGER_UUID_VALUE $WORKFLOW_INPUTS
+
+wait_for_status $TRIGGER_UUID_VALUE $WORKFLOW_ARTIFACTS_URL $WAIT_TIMEOUT_MINUTES
 #END
